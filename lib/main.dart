d85f2fa8de484ae9a235fc1d1dc1f178eb7f72a2
SKULL94 <zeeshanhaider794@gmail.com>
2025-04-27 22:02:32 +0530

    responsive UIs integrated

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:skin_sync/app.dart';
-import 'package:skin_sync/notification_service.dart';
+import 'package:skin_sync/utils/notification_service.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
